49e9fc813651d1ccb267b812b58dfc8ee7b1fcc1
JanMouwes <jan.mouwes@gmail.com>
2020-12-31 17:09:27 +0100

    Version 1 of personal site.

---
+++ src/App.vue
@@ -0,0 +1,73 @@
+<template>
+    <div id="app">
+        <div style="margin:0 25%">
+            <div class="d-flex">
+                <div>
+                    <h3>Welcome</h3>
+                    <p>{{ content.welcome }}</p>
+
+                    <h5>Contact</h5>
+                    <ul class="list-unstyled">
+                        <li><a :href="contact.linkedin">LinkedIn</a></li>
+                        <li><a :href="contact.github">GitHub</a></li>
+                    </ul>
+                </div>
+
+                <div>
+                    <img
+                        src="https://github.com/janmouwes.png?size=100"
+                        alt="portrait of Jan"
+                        class="rounded-circle shadow"
+                    />
+                </div>
+            </div>
+            <div>
+                <h3>Projects</h3>
+                <p>{{ content.projects }}</p>
+                <div class="d-flex flex-wrap justify-content-between">
+                    <ProjectCard
+                        v-for="proj in projects"
+                        class="mb-3"
+                        style="width: 225px"
+                        :key="proj['title']"
+                        :project-title="proj['title']"
+                        :description="proj['description']"
+                        :url="proj['url']"
+                        :source-url="proj['source-url']"
+                    />
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import ProjectCard from './components/ProjectCard.vue'
+import projectData from "./content/projects.json"
+import contentData from "./content/content.json"
+import contactData from "./content/contact.json"
+
+export default {
+    name: 'App',
+    components: {
+        ProjectCard
+    },
+    data() {
+        return {
+            projects: projectData,
+            content: contentData,
+            contact: contactData,
+        }
+    }
+}
+</script>
+
+<style>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="card">
+        <div class="card-body">
+            <h5 class="card-title">{{ this.projectTitle }}</h5>
+            <p class="card-text" :active="this.description != null">{{ this.description }}</p>
+            <ul class="list-unstyled">
+                <li><a :href='this.url' :active="this.url != null">Link</a></li>
+                <li><a :href='this.sourceUrl' :active="this.sourceUrl != null">Source</a></li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+import "bootstrap/dist/css/bootstrap.css"
+
+export default {
+    name: "ProjectCard",
+    props: {
+        projectTitle: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String
+        },
+        url: {
+            type: String
+        },
+        sourceUrl: {
+            type: String
+        }
+    }
+}
+</script>
+
+<style scoped>
+a {
+    white-space: nowrap;
+    text-overflow: clip;
+    display: block;
+    overflow: hidden;
+}
+</style>
